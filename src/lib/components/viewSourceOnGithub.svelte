884ffebe81bd93d06814473cb9d26dc92e1d2640
Jef Meijvis <jef.meijvis@hotmail.com>
2024-01-15 11:24:00 +0100

    Added comment section and github reference to taxonomy

---
+++ src/lib/components/viewSourceOnGithub.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    export let path : string = ""
+
+    let base : string = "https://github.com/jefmeijvis/cornucopia.dotnetlab.eu/blob/master/";
+    let href : string = base + path;
+</script>
+
+<div>
+    <a target="_blank" {href}>
+        <img alt="Github logo" src="/icons/github.png"/>
+        <span>View source on GitHub</span>
+    </a>
+</div>
+
+<style>
+    a
+    {
+        background: none;
+        border: 1px white solid;
+        display: flex;
+        flex-shrink: 1;
+        color:var(--color-text-primary);
+        font-family: 'Courier New', Courier, monospace;
+        background-color: var(--background);
+        border-radius: .5rem;
+        margin:auto;
+        padding: .5rem;
+        margin-top: 4rem;
+        cursor:pointer;
+        width:max-content;
+        text-decoration: none;
+        
+    }
+
+    a:hover
+    {
+        color: #00d100;
+        border: 1px rgb(0, 209, 0) solid;
+
+    }
+
+    a:active
+    {
+        font-weight: bold;
+    }
+
+    span
+    {
+        padding: .5rem;
+    }
+   
+    img
+    {
+        width : 2rem;
+    }
+  </style>
--- src/routes/taxonomy/[category]/[entry]/+page.svelte
+++ src/routes/taxonomy/[category]/[entry]/+page.svelte
@@ -3,7 +3,11 @@
     import { Text } from '$lib/utils/text';
     import SvelteMarkdown from 'svelte-markdown'
     import renderers from '$lib/components/renderers/renderers';
+    import ViewSourceOnGithub from "$lib/components/viewSourceOnGithub.svelte"
+    import Utterances from "$lib/components/utterances.svelte"
 </script>
 
 <h1>{Text.Format(data.category)} - {Text.Format(data.entry)}</h1>
 <SvelteMarkdown {renderers} source={data.markdown}></SvelteMarkdown>
+<ViewSourceOnGithub path="data/taxonomy/{data.category}/{data.entry}/index.md"></ViewSourceOnGithub>
+<Utterances name={data.category + '-' + data.entry}></Utterances>
