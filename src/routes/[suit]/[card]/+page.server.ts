0d1d60e8ce1c45ea1efaf79a8286d7ceda7fcd0c
Ward Impe <ward.impe@dotnetlab.eu>
2024-02-16 14:54:48 +0100

    clean up + fix link for range

--- src/routes/[suit]/[card]/+page.server.ts
+++ src/routes/[suit]/[card]/+page.server.ts
@@ -1,11 +1,14 @@
-import { FileSystemHelper } from '$lib/filesystem/fileSystemHelper';
-import { getCardBySuitAndName, getCardsFlat } from '../../../domain/card/cardController';
-import type { PageServerLoad } from './$types';
- 
+import { FileSystemHelper } from "$lib/filesystem/fileSystemHelper";
+import {
+  getCardBySuitAndName,
+  getCardsFlat,
+} from "../../../domain/card/cardController";
+import type { PageServerLoad } from "./$types";
+
 export const load = (({ params }) => {
   return {
-    card : getCardBySuitAndName(params.suit, params.card),
-    cards : getCardsFlat(),
-    ASVSRoutes : FileSystemHelper.getRouteStructure(),
+    card: getCardBySuitAndName(params.suit, params.card),
+    cards: getCardsFlat(),
+    ASVSRoutes: FileSystemHelper.ASVSRouteMap(),
   };
 }) satisfies PageServerLoad;
